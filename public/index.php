9e552fb1a21fc0c41947b8503f24463bf5c6cc40
Armin Hopp <armin.hopp@check24.de>
2021-10-13 21:38:21 +0200

    Move config into config file. Introduce EpicStruct

--- public/index.php
+++ public/index.php
@@ -3,40 +3,46 @@
 include dirname(__DIR__) . "/vendor/autoload.php";
 use JiraRestApi\Issue\IssueService;
 
+class EpicStruct extends \stdClass
+{
+    public string $key;
+    public string $itLeadName;
+    public string $pmLeadName;
+    public string $summary;
+
+    private function __construct() {}
+
+    public static function fromIssue(JiraRestApi\Issue\Issue $issue): self {
+        $struct = new self;
+        $struct->key = $issue->key;
+        $struct->itLeadName = $issue->fields->getCustomFields()['customfield_10603']->displayName ?? 'no it lead set';
+        $struct->pmLeadName = $issue->fields->assignee->displayName ?? 'no pm lead set';
+        $struct->summary = $issue->fields->summary ?? 'no summary set';
+
+        return $struct;
+    }
+}
+
+
 try {
+    $config = include '../config.php';
     $issueService = new IssueService();
 
-    $fields = [
-        // default: '*all'
-        'summary',
-        'assignee',
-        'developer',
-        'customfield_10006',
-        'customfield_10603',
-        'key',
-    ];
-    $expand = [];
-
-    $fieldNameMap = [
-        'customfield_10603' => 'IT Lead',
-        'assignee' => 'PM Lead',
-        'summary' => 'Projekt',
-        'key' => 'Epic',
-    ];
-
     #$issuesResult = $issueService->search('filter=64306', 0,50, $fields, $expand);
-    $issuesResult = $issueService->search('key=HOAPI-23762', 0,50, $fields, $expand);
+    $issuesResult = $issueService->search('filter=64306', 0,50, ['key'], []);
 
     $issueDetails = [];
     foreach ($issuesResult->issues as $issue) {
-        $key = $issue->key;
-        $issueDetails[$key] = $issueService->get($key, ['fields' => ['*all'], 'expand' => $expand]);
-        break;
+        try {
+            $key = $issue->key;
+            $issueDetails[$key] = EpicStruct::fromIssue($issueService->get($key, ['fields' => $config->fields, 'expand' => $config->expand]));
+        } catch (\Throwable $ex) {
+            // ignore for now
+        }
     }
 
     header('Content-Type: application/json');
     echo json_encode([
-        #'issues' => $issuesResult,
         'issueDetails' => $issueDetails,
     ]);
 } catch (JiraRestApi\JiraException $e) {
